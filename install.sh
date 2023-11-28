0b8d6a28989693384c0313e7afc5d9c8607be3f2
Shad Humydee <showmmos@gmail.com>
2023-11-29 00:59:35 +0600

    for run at startup copy file to UserAgents dir

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ brew install -v blueutil
 
 mkdir -v ~/scripts
 cp -f soundsentry.sh ~/scripts/
-sudo cp -f com.shad.soundsentry.plist /Library/LaunchDaemons
+sudo cp -f com.shad.soundsentry.plist /Library/LaunchAgents
 
 echo "Please logout and log back in!"
